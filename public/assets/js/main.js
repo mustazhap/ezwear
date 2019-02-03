5721f1cbec5c5f7dfb48a08e4561a89594a1f54f
 Rustem Mustazhap <russt@MacBook-Pro-Mustazhap.local>
2019-02-03 21:47:51 +0600

    ne bar sol

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -50,6 +50,8 @@ $('.popular-owl').owlCarousel({
 
 
 // rating
+if($("#new-rank1").length){
+
 var starRating2 = raterJs( {
     max:5, 
     rating:4.4, 
@@ -134,24 +136,57 @@ var starRating2 = raterJs( {
         done(); 
     }
 }); 
+    var starRating2 = raterJs( {
+        max:5, 
+        rating:4.4, 
+        element:document.querySelector("#new-rank1"), 
+        disableText:"Custom disable text!", 
+        ratingText:"My custom rating text {rating}", 
+        showToolTip:true, 
+        starSize: 12,
+        rateCallback:function rateCallback(rating, done) {
+            starRating2.setRating(rating); 
+            starRating2.disable(); 
+            done(); 
+        }
+    }); 
 
-var starRating2 = raterJs( {
-    max:5, 
-    rating:4.4, 
-    element:document.querySelector("#new-rank1"), 
-    disableText:"Custom disable text!", 
-    ratingText:"My custom rating text {rating}", 
-    showToolTip:true, 
-    starSize: 12,
-    rateCallback:function rateCallback(rating, done) {
-        starRating2.setRating(rating); 
-        starRating2.disable(); 
-        done(); 
-    }
-}); 
+
+    var starRating2 = raterJs( {
+        max:5, 
+        rating:4.4, 
+        element:document.querySelector("#new-rank2"), 
+        disableText:"Custom disable text!", 
+        ratingText:"My custom rating text {rating}", 
+        showToolTip:true, 
+        starSize: 12,
+        rateCallback:function rateCallback(rating, done) {
+            starRating2.setRating(rating); 
+            starRating2.disable(); 
+            done(); 
+        }
+    }); 
+}
+
+if($("#item-rank1").length){
+    var starRating2 = raterJs( {
+        max:5, 
+        rating:4.4, 
+        element:document.querySelector("#item-rank1"), 
+        disableText:"Custom disable text!", 
+        ratingText:"My custom rating text {rating}", 
+        showToolTip:true, 
+        starSize: 12,
+        rateCallback:function rateCallback(rating, done) {
+            starRating2.setRating(rating); 
+            starRating2.disable(); 
+            done(); 
+        }
+    }); 
+}
 
 // popular items
-$(".popular__item").hover(function(){
+$(".ez-popular__item").hover(function(){
     var a = $(this).find("img").attr("rel");
     var b = $(this).find("img").attr("src");
     $(this).find("img").attr("src", a);
@@ -163,6 +198,56 @@ $(".popular__item").hover(function(){
 // Маска для телефона
 $(".mask").mask("+7 (999) 999 99 99");
 
+
+// Select menu
+
+$('select').each(function(){
+    var $this = $(this), numberOfOptions = $(this).children('option').length;
+  
+    $this.addClass('select-hidden'); 
+    $this.wrap('<div class="select"></div>');
+    $this.after('<div class="select-styled"></div>');
+  
+    var $styledSelect = $this.next('div.select-styled');
+    $styledSelect.text($this.children('option').eq(0).text());
+  
+    var $list = $('<ul />', {
+        'class': 'select-options'
+    }).insertAfter($styledSelect);
+  
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+  
+    var $listItems = $list.children('li');
+  
+    $styledSelect.click(function(e) {
+        e.stopPropagation();
+        $('div.select-styled.active').not(this).each(function(){
+            $(this).removeClass('active').next('ul.select-options').hide();
+        });
+        $(this).toggleClass('active').next('ul.select-options').toggle();
+    });
+  
+    $listItems.click(function(e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+        //console.log($this.val());
+    });
+  
+    $(document).click(function() {
+        $styledSelect.removeClass('active');
+        $list.hide();
+    });
+  
+  });
+  
+  // 
   // Menu
   $(".jobs__menu").click(function(){
     if($(this).hasClass("animated")){
